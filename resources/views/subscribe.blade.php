fe797c33950152c8e38ea35f1c0aefbf31f54829
mahdy zahran <ali-mahdy@windowslive.com>
2023-04-11 17:47:53 +0300

    minor fix to subscription page

--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -1,8 +1,33 @@
-<h1>You must subscribe first</h1>
 
-@php
-$currentUser = auth()->user();
-@endphp
 
-<li><a class="nav-link" href="{{ route('user.edit',['id'=>$currentUser->id]) }}">My Companies</a></li>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Subscribe</div>
+                
+                <div class="card-body">
+                        <div class="card" style="width: 100%;">
+                        <div class="card-body">
+                        <div class="alert alert-primary" role="alert">
+                            You must subscribe first
+                            </div> 
+
+                        @php
+                        $currentUser = auth()->user();
+                        @endphp
+
+                        <a class="btn btn-primary" href="{{ route('user.edit',['id'=>$currentUser->id]) }}">Subscribe</a>
+                        </div>
+                        </div>                    
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
 
